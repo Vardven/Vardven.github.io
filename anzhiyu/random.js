a91c355d2c34e536bf5b42642122fecf2573fc89
Vardven <vardven@gmail.com>
2024-07-08 01:29:52 +0800

    Site updated: 2024-07-08 01:29:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/04/hello-world/","2024/07/04/欢迎来到我的博客！/"];function toRandomPost(){
+var posts=["2024/07/08/安装主题后发表的第一篇测试文章/","2024/07/04/欢迎来到我的博客！/","2024/07/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
