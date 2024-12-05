287a100a0c25c45f2655159df360159ef3d656f1
vardven <vardven@gmail.com>
2024-12-05 20:21:18 +0800

    Site updated: 2024-12-05 20:21:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/ROS和ROS2的区别/","undefined/hello-world/","undefined/美化Hexo博客，主题安装配置/"];function toRandomPost(){
+var posts=["undefined/ROS和ROS2的区别/","undefined/美化Hexo博客，主题安装配置/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
