74a78d9c69fa13d30d5f7e8809c90e7555788378
vardven <vardven@gmail.com>
2025-03-21 11:41:32 +0800

    Site updated: 2025-03-21 11:41:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/ROS和ROS2的区别/","undefined/word常用快捷键/","undefined/电焊分类/","undefined/美化Hexo博客，主题安装配置/"];function toRandomPost(){
+var posts=["undefined/ROS和ROS2的区别/","undefined/word常用快捷键/","undefined/修复焊接咬边：美化焊缝的外观，提升焊缝的强度和耐久性/","undefined/电焊分类/","undefined/美化Hexo博客，主题安装配置/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
